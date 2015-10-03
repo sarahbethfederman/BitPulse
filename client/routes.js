bab0d82fe7d4df3effc86f956c11ccc8773334ec
Julian Kingman <julian@trainerengine.com>
2015-10-03 18:06:09 -0400

    routes

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,28 @@
 FlowRouter.route('/', {
-  action: function() {
-    BlazeLayout.render("masterLayout", {content: "sessions"});
-  }
+    name: 'Workouts',
+    action: function () {
+        BlazeLayout.render("masterLayout", {content: "workouts"});
+    }
 });
+
+FlowRouter.route('/workouts/:workoutId',{
+    name: 'Exercises',
+    action: function(){
+        BlazeLayout.render('masterLayout', {content: 'exercise'});
+    }
+});
+
+FlowRouter.route('/sessions/:sessionId', {
+    name: 'SingleSession',
+    action: function () {
+        BlazeLayout.render('masterLayout', {content: 'session'});
+    }
+});
+
+FlowRouter.route('/workouts', {
+    name: 'Sessions',
+    action: function(){
+        BlazeLayout.render('masterLayout', 'sessions');
+    }
+});
+
