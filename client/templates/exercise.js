26ed10401f72cac7dcaec798d97da536f5268461
Julian Kingman <Julian@Michelles-MacBook-Air.local>
2015-10-03 22:23:39 -0400

    added all the fake data

--- client/templates/exercise.js
+++ client/templates/exercise.js
@@ -1,25 +1,55 @@
-Template.exercise.helpers({
-    workout: function(){
-        var workoutId = FlowRouter.getParam('workoutId');
-        return Workouts.findOne({_id: workoutId});
-    },
-    exercises: function(workout){
-        return workout.exercises;
+Template.exercise.onRendered(function() {
+  Session.setDefault('play', false);
+  Session.setDefault('timeSpent', 0);
+  Tracker.autorun(function() {
+    if (Session.get('play')) {
+      // var duration = Session.get('currentSession').duration;
+      function play() {
+        var progress = Session.get('timeSpent') * 100 / duration;
+        Session.set('timeSpent', Session.get('timeSpent') + 1);
+        $('progress').val(progress);
+        setTimeout(play, 1000);
+        console.log('playing for ', timeSpend, 'seconds.');
+      }
+      play();
     }
+  });
+});
+
+Template.exercise.helpers({
+  workout: function() {
+    var workoutId = FlowRouter.getParam('workoutId');
+    return Workouts.findOne({
+      _id: workoutId
+    });
+  },
+  exercises: function(workout) {
+    var exercises = _.sortBy(workout.exercises, 'order');
+    return exercises;
+    Session.set('currentExercise', exercises[0]);
+  }
 });
 
 Template.exercise.events({
-    'submit #newExercise': function(e,t){
-        var workout = this.exercises;
-        var name = e.target.name;
-        var duration = parseInt(e.target.duration);
-        workout.push('exercises', {
-            name: name,
-            duration: duration
-        });
-        Meteor.call('saveDoc', workout);
-    },
-    'click .playWorkout': function(e,t){
-        
+  'submit #newExercise': function(e, t) {
+    var workout = this.exercises;
+    var name = e.target.name;
+    var duration = parseInt(e.target.duration);
+    workout.push('exercises', {
+      name: name,
+      duration: duration,
+      //make the order the highest order
+      order: _.max(this.exercises, function(exercise) {
+        return exercise.order;
+      }).order + 1
+    });
+    Meteor.call('saveDoc', workout);
+  },
+  'click .playPauseWorkout': function(e, t) {
+    if (Session.get('play')) {
+      Session.set('play', false);
+    } else {
+      Session.set('play', true);
     }
+  }
 });
