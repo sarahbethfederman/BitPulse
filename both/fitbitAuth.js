141cebfedff4aecd1bd153efe6d060bab784b2fc
Julian Kingman <Julian@Michelles-MacBook-Air.local>
2015-10-04 11:49:27 -0400

    fitbit auth

--- both/fitbitAuth.js
+++ both/fitbitAuth.js
@@ -1,8 +1,8 @@
-// ServiceConfiguration.configurations.remove({
-//     service: "fitbit"
-// });
-// 
-// ServiceConfiguration.configurations.upsert(
-//     { service: "fitbit" },
-//     { $set: { consumerKey: '18398fc7a196fce2495f174166cce717', secret: '58f05ffbe9edcd2ba2fc48a5448e1f05' } }
-// );
+ServiceConfiguration.configurations.remove({
+    service: "fitbit"
+});
+
+ServiceConfiguration.configurations.upsert(
+    { service: "fitbit" },
+    { $set: { consumerKey: '18398fc7a196fce2495f174166cce717', secret: '58f05ffbe9edcd2ba2fc48a5448e1f05' } }
+);
