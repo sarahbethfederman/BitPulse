37dea972f3e665876b5d5ce212d965ef557384d9
Julian Kingman <Julian@Michelles-MacBook-Air.local>
2015-10-04 10:39:01 -0400

    average hb

--- both/collections/sessionsCollection.js
+++ both/collections/sessionsCollection.js
@@ -1,22 +1,33 @@
 Sess = new Mongo.Collection('sessions');
 
 Sess = Astro.Class({
-    name: 'Sess',
-    collection: Sess,
-    fields: {
-        workout: 'string',
-        'exercises.$.ex_id': 'string',
-        'exercises.$.data.$.hr': 'number',
-        'exercises.$.data.$.timeStamp': 'date'
-    },
-    events: {
+  name: 'Sess',
+  collection: Sess,
+  fields: {
+    workout: 'string',
+    'exercises.$.ex_id': 'string',
+    'exercises.$.data.$.hr': 'number',
+    'exercises.$.data.$.timeStamp': 'date'
+  },
+  events: {
 
+  },
+  methods: {
+    thisUser: function() {
+      return Meteor.users.find({
+        _id: this.owner
+      });
     },
-    methods: {
-        thisUser: function(){
-            return Meteor.users.find({_id: this.owner});
-        }
+    averageHeartrate: function() {
+      var elmt = _.pluck(_.flatten(_.pluck(this.exercises, 'data')),'hr');
+      var sum = 0;
+      for (var i = 0; i < elmt.length; i++) {
+        sum += parseInt(elmt[i], 10); //don't forget to add the base
+      }
+      console.log(elmt);
+      return sum/elmt.length;
     }
+  }
 });
 
 //Workouts.insert({somefield: 'some value'});
--- both/methods/workoutMethods.js
+++ both/methods/workoutMethods.js
@@ -14,13 +14,22 @@ Meteor.methods({
     var newExercise = [];
     workout.exercises.forEach(function(child, i) {
       newExercise[i] = workout.exercises[i];
-      newExercise[i].order = workout.exercises[i].order - 1;
+      // newExercise[i].order = workout.exercises[i].order - 1;
+      _.set(newExercise[i], 'order', workout.exercises[i].order - 1);
+      console.log(child,i,workout.exercises[i].order, 'the old order value, then the new: ', workout.exercises[i].order - 1);
     });
-    // console.log(maxOrder, newExercise);
-    var next = _.chain(newExercise).pluck("order").indexOf(-1).value();
-    // console.log(next);
-    newExercise[next].order = maxOrder;
+    var last = _.findIndex(newExercise, function(ex) {
+      return ex.order == -1;
+    });
+    var next = _.findIndex(newExercise, function(ex){
+      return ex.order == 0;
+    });
+    console.log('old/new', workout.exercises, newExercise);
+    newExercise[last].order = maxOrder;
+    console.log('last', newExercise[last]);
+    console.log('next', newExercise[next]);
     workout.set('exercises', newExercise);
     Meteor.call('saveDocs', workout);
+    return newExercise[next];
   }
-})
+});
